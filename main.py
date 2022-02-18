dc2b70001c0e54d435bdad54dbdff567add2d664
SatyajeetBehera1 <86057919+SatyajeetBehera1@users.noreply.github.com>
2022-02-19 01:02:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Satyajeet Behera')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x = 'satay'
+for i in x:
+    print(i)
+
+x = ['satya', 90, 8.8]
+for i in x:
+    print(i)
+
+x = ['satya 1', 'rahul 2', 'raj 3']
+for i in x:
+    print(i)
+
+for i in range(1, 21):
+    if i % 6 != 0:
+        print(i)
+
+i = 1
+while i <= 5:
+    print('satay', end='')
+    j = 1
+    while j <= 5:
+        print('rocks', end='')
+        j = j + 1
+    i = i + 1
+    print('')
